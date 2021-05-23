cf8817e59e9a6fb364b1baed39048d4303bc1c57
Will Lane <williamlane923@gmail.com>
2021-05-23 16:53:29 -0700

    init and new to start projects

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use clap::Clap;
 
 mod cli;
 use cli::*;
-use std::env;
+use std::{env, fs};
 use std::{fs::File, io::Write};
 
 fn main() {
@@ -11,27 +11,39 @@ fn main() {
     match &cli.cmd {
         Cmd::Init(Init { name, .. }) => {
             File::create("CMakeLists.txt")
-                .and_then(|mut file| {
-                    file.write_all(
-                        format!(
-                            "cmake_minimum_required(VERSION 3.20.2)\nproject({} C)",
-                            name.as_ref().unwrap_or(
-                                &env::current_dir()
-                                    .expect("Could not get working dir")
-                                    .file_name()
-                                    .unwrap()
-                                    .to_str()
-                                    .unwrap()
-                                    .to_string()
-                            )
-                        )
-                        .as_bytes(),
-                    )
-                })
+                .and_then(|file| Ok(base_cmake(file, name)))
                 .expect("Could not create file");
         }
-        Cmd::New(New { name, .. }) => println!("New: {}", name),
+        Cmd::New(New { name, .. }) => {
+            match fs::create_dir(name) {
+                Err(error) => panic!("Error: {}", error.to_string()),
+                Ok(_) => {}
+            }
+
+            File::create(format!("{}/CMakeLists.txt", name))
+                .and_then(|file| Ok(base_cmake(file, &Some(name.to_owned()))))
+                .expect("Could not create file");
+        }
     }
 
     println!("{:?}", cli);
+}
+
+fn base_cmake(mut file: File, name: &Option<String>) {
+    file.write_all(
+        format!(
+            "cmake_minimum_required(VERSION 3.20.2)\nproject({} C)",
+            name.as_ref().unwrap_or(
+                &env::current_dir()
+                    .expect("Could not get working dir")
+                    .file_name()
+                    .unwrap()
+                    .to_str()
+                    .unwrap()
+                    .to_string()
+            )
+        )
+        .as_bytes(),
+    )
+    .expect("Could not write to file");
 }
